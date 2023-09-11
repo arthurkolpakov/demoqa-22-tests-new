7bfe43d16f7c5a703a1ec2c367b548c282123604
Artur <kolpakovartur91@gmail.com>
2023-09-11 20:35:38 +0300

    added .build.gradle and FormTest.java

---
+++ src/test/java/FormTest.java
@@ -0,0 +1,36 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+public class FormTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/automation-practice-form");
+
+        $("#firstName").setValue("Artur");
+        $("#lastName").setValue("Kolpakov");
+        $("#userEmail").setValue("kolpakovartur91@gmail.com");
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber-label").setValue("9003308519");
+
+
+
+    }
+
+    }
