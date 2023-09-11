19bd28d0a09be809d158a9c70688cb48a928a98d
Artur <kolpakovartur91@gmail.com>
2023-09-11 22:47:07 +0300

    update FormTest.java

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -32,6 +32,7 @@ public class FormTest {
         //Пока не знаю как в папку resources добавить картинку
         $("#currentAddress-label").setValue("Cheboksary");
         $("react-select-3-input]").setValue("NCR").click();
+
         $("#submit").click();
 
 
