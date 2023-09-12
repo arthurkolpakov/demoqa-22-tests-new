ea9434d89d8824aee7ecebcb7335295423ce3e54
Artur <kolpakovartur91@gmail.com>
2023-09-12 19:25:07 +0300

    project finished

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -4,12 +4,13 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class FormTest {
     @BeforeAll
     static void beforeAll() {
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = false;
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
     }
@@ -17,38 +18,39 @@ public class FormTest {
     @Test
     void fillFormTest() {
         open("/automation-practice-form");
-
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
         $("#firstName").setValue("Artur");
         $("#lastName").setValue("Kolpakov");
         $("#userEmail").setValue("kolpakovartur91@gmail.com");
-        $("#genterWrapper").$(byText("Male")).click();
-        $("#userNumber-label").setValue("9003308519");
-        $("#dateOfBirth-label").setValue("02 Apr 1991");
-        $("react-datepicker__month-select").selectOption("April");
-        $("react-datepicker__year-select").selectOption("1991");
-        $("react-datepicker__day react-datepicker__day--002 react-datepicker__day--selected").click();
-        $("#subjects-label").setValue("QA");
-        $("label[for=hobbies-checkbox-1]").click();
+        $(byText("Male")).click();
+        $("#userNumber").setValue("9003308519");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-dropdown-container").$(byText("April")).click();
+        $(".react-datepicker__year-select").selectOption("1991");
+        $(".react-datepicker__day--002").click();
+        $("#subjectsInput").setValue("Q A").pressEnter();
+        $(byText("Sports")).click();
         $("#uploadPicture").uploadFromClasspath("101.jpg");
-        $("#currentAddress-label").setValue("Cheboksary");
-        $("react-select-3-input]").setValue("NCR").click();
+        $("#currentAddress").setValue("Moscow");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
         $("#submit").click();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        $(".modal-dialog").shouldHave(text("Thanks for submitting the form"));
+        $(".modal-content").shouldHave(
+                text("Artur Kolpakov"),
+                text("kolpakovartur91@gmail.com"),
+                text("Male"),
+                text("9003308519"),
+                text("2 April,1991"),
+                text("Q A"),
+                text("101.jpg"),
+                text("Moscow"),
+                text("NCR Delhi")
+        );
     }
-
-    }
+}
